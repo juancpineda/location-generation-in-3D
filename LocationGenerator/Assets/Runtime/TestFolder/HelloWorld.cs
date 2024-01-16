24c0e6d0aca6c823849d459e3f2871b620ff7137
juancarlos@sdsharp <juancpineda2010@gmail.com>
2024-01-16 18:22:36 -0500

    Update Package
    Update Package

---
+++ LocationGenerator/Assets/Runtime/TestFolder/HelloWorld.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace LocationGen
+{
+    public class HelloWorld : MonoBehaviour
+    {
+        private void Start()
+        {
+            Debug.Log("Hello World");
+        }
+    }
+}
+
+
